31eb6ee591bbd1bb2b343ca5c906cbe823b0b703
Dewmi <dewmidesadi@gmail.com>
2026-06-06 15:40:27 +0530

    Initial Commit

---
+++ iosapplication1/iosapplication1App.swift
@@ -0,0 +1,17 @@
+//
+//  iosapplication1App.swift
+//  iosapplication1
+//
+//  Created by student5 on 2026-06-06.
+//
+
+import SwiftUI
+
+@main
+struct iosapplication1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
